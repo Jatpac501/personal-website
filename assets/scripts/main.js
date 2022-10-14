dc54f8c85c82d970619fbd8e41823b125583ff8b
hrdfx <48427098+Jatpac501@users.noreply.github.com>
2022-10-15 00:01:01 +0400

    Add files via upload

---
+++ assets/scripts/main.js
@@ -0,0 +1,11 @@
+console.log("JS: Start"); // Начало
+//
+let InfoTitle = document.querySelector('title').innerText;
+let InfoCSS = document.querySelector('link').href;
+let InfoScript = document.querySelector('script').src;
+console.log("Title -- " + InfoTitle);
+console.log("CSS -- " + InfoCSS);
+console.log("JS -- " + InfoScript);
+//
+//
+console.log("JS: Stop"); // Конец
